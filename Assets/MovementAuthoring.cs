95a1c613b8dbb3527cf1c6a8993503b3bdbb217c
Brendan Nelligan <nelligan81@gmail.com>
2020-02-01 11:35:39 -0500

    Starting scripts and player primitive

---
+++ Assets/MovementAuthoring.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Unity.Entities;
+
+namespace Assets
+{
+
+
+    class FreeMovementInputSystem : ComponentSystem
+    {
+        EntityQuery freeMoveQuery;
+        protected override void OnUpdate()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
