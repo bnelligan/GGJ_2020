fec993accbfc3aa1e2dddfce96a521214884d89b
Brendan Nelligan <nelligan81@gmail.com>
2020-02-01 18:04:09 -0500

    Fix temp compile error

--- Assets/GGJ 2020/Scripts/HealthSystem.cs
+++ Assets/GGJ 2020/Scripts/HealthSystem.cs
@@ -20,17 +20,36 @@ namespace BrokenBattleBots
         float Max;
     }
 
+    /// <summary>
+    /// Health loss per turn
+    /// </summary>
     struct HealthDecay : IComponentData
     {
         float DecayAmount;
         float DecayTimer;
     }
 
-    class HealthSystem : JobComponentSystem
-    {
-        protected override JobHandle OnUpdate(JobHandle inputDeps)
-        {
+    /// <summary>
+    /// Damage taken by an entity. Removed after it is applied to Health.
+    /// </summary>
+    struct Damage : IComponentData { float Value; }
 
-        }
-    }
+    //class HealthSystem : JobComponentSystem
+    //{
+    //    EndSimulationEntityCommandBufferSystem ecb_EndSim;
+
+    //    protected override void OnCreate()
+    //    {
+    //        base.OnCreate();
+    //        ecb_EndSim = World.GetOrCreateSystem<EndSimulationEntityCommandBufferSystem>();
+    //    }
+
+    //    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    //    {
+
+    //    }
+        
+    //    struct ApplyDamageJob : IJobForEach<Health, Damage>
+        
+    //}
 }
