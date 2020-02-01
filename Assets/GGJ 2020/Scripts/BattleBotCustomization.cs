60f18926bb24b49aed3a03d9f43ee0a2b63d6c50
chrisnobrega <nobrega.chris@outlook.com>
2020-02-01 17:07:51 +0100

    added chris's scene

---
+++ Assets/GGJ 2020/Scripts/BattleBotCustomization.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace BrokenBattleBots
+{
+    public class BattleBotCustomization : MonoBehaviour
+    {
+        public enum NodeType
+        {
+            Head,
+            ArmLeft,
+            ArmRight,
+            ShoulderLeft,
+            ShoulderRight,
+            Chest,
+            Bottom,
+        }
+
+        public Transform nodeHead;
+        public Transform nodeArmLeft;
+        public Transform nodeArmRight;
+        public Transform nodeBottom;
+        public Transform nodeShoulderLeft;
+        public Transform nodeShoulderRight;
+
+        private void Update ()
+        {
+            
+        }
+    }
+}
