12755798abc835cfaf4a61905a4959a6f81a45ba
zikiro <mrzombiekilla24@yahoo.com>
2020-02-01 22:11:12 -0500

    EMP Strike
    - Script which keeps track of all enemies within tower range and when tower hits 0 health they all get deleted.

---
+++ Assets/GGJ 2020/Scripts/ByeByeBirdy/ByeBye.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ByeBye : MonoBehaviour
+{
+    public List<GameObject> EnemiesWithinRange = new List<GameObject>();
+    public int health;
+
+    public void Update()
+    {
+        if (health <= 0)
+        {
+            foreach (var item in EnemiesWithinRange)
+            {
+                EnemiesWithinRange.Remove(item);
+                Destroy(item);
+            }
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Enemy")
+        {
+            EnemiesWithinRange.Add(other.gameObject);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Enemy")
+        {
+            EnemiesWithinRange.Remove(other.gameObject);
+        }
+    }
+
+}
