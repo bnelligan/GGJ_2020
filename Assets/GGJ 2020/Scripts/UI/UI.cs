d09d20b37b2e97f748e41a5a8a83fdb11a245454
blaj95 <32546217+blaj95@users.noreply.github.com>
2020-02-02 15:22:45 -0500

    UI changes

---
+++ Assets/GGJ 2020/Scripts/UI/UI.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using BrokenBattleBots;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI : MonoBehaviour
+{
+    public Image gen1Fill;
+
+    public Image gen2Fill;
+
+    public Image gen3Fill;
+
+    public Image healthBarFill;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        gen1Fill.fillAmount = (float)GUI_Info.Gen1HP_Current / GUI_Info.Gen1HP_Max;
+        gen2Fill.fillAmount = (float)GUI_Info.Gen2HP_Current / GUI_Info.Gen2HP_Max;
+        gen3Fill.fillAmount =(float) GUI_Info.Gen3HP_Current / GUI_Info.Gen3HP_Max;
+        healthBarFill.fillAmount = (float)GUI_Info.PlayerHP_Max - GUI_Info.PlayerHP_Current;
+    }
+}
