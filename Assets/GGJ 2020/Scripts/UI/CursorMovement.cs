da15a628e94857e7decbbc9d12c19a4b33526447
blaj95 <32546217+blaj95@users.noreply.github.com>
2020-02-02 03:21:05 -0500

    default cursor

---
+++ Assets/GGJ 2020/Scripts/UI/CursorMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorMovement : MonoBehaviour
+{
+    public Camera mainCamera;
+
+    public Transform cursor;
+
+    public Texture2D cursorTexture;
+    public CursorMode cursorMode = CursorMode.Auto;
+    public Vector2 hotSpot = Vector2.zero;
+    
+    void Start()
+    {
+        // Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        // Vector3 mousePos = mainCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10.0f));
+        // cursor.position = mousePos;
+        
+    }
+}
