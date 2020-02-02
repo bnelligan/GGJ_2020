4e2334a82c17d79a3492e29cc947e857304dad93
blaj95 <32546217+blaj95@users.noreply.github.com>
2020-02-02 12:44:42 -0500

    Death system

---
+++ Assets/GGJ 2020/Scripts/DOTS/DOTS_Death.cs
@@ -0,0 +1,29 @@
+﻿using Unity.Entities;
+using Unity.Transforms;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Physics;
+using Unity.Collections;
+using UnityEngine;
+
+
+namespace BrokenBattleBots
+{
+    struct Tag_Generator : IComponentData{}
+
+    
+    class DeathSystem : ComponentSystem
+    {
+        protected override void OnUpdate()
+        {
+            Entities.ForEach((Entity e, ref Tag_Dead dead, ref Tag_Generator deadGenerator) =>
+            {
+                Debug.Log("Dead Gen");
+                EntityManager.DestroyEntity(e);
+            
+            });
+        }
+    }
+    
+}
+
