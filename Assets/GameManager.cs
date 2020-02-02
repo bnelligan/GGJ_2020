2809280de448094ecdc87cac6447bc826c8fd0c5
zikiro <mrzombiekilla24@yahoo.com>
2020-02-02 15:20:22 -0500

    Arena Move And GameManager
    - Moved Arena to correct Height
    - Added GameManager script

---
+++ Assets/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BrokenBattleBots;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject[] spawners;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (BattleBotCustomization.instance.Standing)
+        {
+            foreach (var item in spawners)
+            {
+                item.gameObject.SetActive(true);
+            }
+        }
+        
+    }
+}
