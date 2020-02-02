c89c6e481be2d06e3e2b608fe4a1975292103b72
zikiro <mrzombiekilla24@yahoo.com>
2020-02-01 20:42:06 -0500

    State Machine
    The beginning of a state machine to be used for the game state.

---
+++ Assets/GGJ 2020/Scripts/GameState/GameState.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class GameState
+{
+    protected GameStateController GSController;
+
+    public GameState(GameStateController gsController)
+    {
+        GSController = gsController;
+    }
+
+    public virtual IEnumerator RepairState()
+    {
+        yield break;
+    }
+
+    public virtual IEnumerator BattleState()
+    {
+        yield break;
+    }
+
+    public virtual IEnumerator GameOverState()
+    {
+        yield break;
+    }
+
+}
---
+++ Assets/GGJ 2020/Scripts/GameState/GameStateMachine.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class GameStateMachine : MonoBehaviour
+{
+    protected GameState State;
+
+    public void SetState(GameState state)
+    {
+        State = state;
+        StartCoroutine(State.RepairState());
+    }
+}
