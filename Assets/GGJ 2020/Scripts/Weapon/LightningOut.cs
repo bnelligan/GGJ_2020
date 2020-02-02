0ee4eb96e07f6873c2f46909d8c47fbf28e1cf1b
zikiro <mrzombiekilla24@yahoo.com>
2020-02-02 01:46:24 -0500

    Bullet that shoots bullets
    - Added a bullet which when it comes in contact with an enemy creates bullets which seek for all enemies within radius.

---
+++ Assets/GGJ 2020/Scripts/Weapon/LightningOut.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightningOut : MonoBehaviour
+{
+    public GameObject miniBullet;
+    public GameObject FirstEnemy;
+    private bool explode;
+    private float timeToExplosion;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeToExplosion = .25f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (explode && timeToExplosion > 0)
+        {
+            timeToExplosion -= Time.deltaTime;
+        }
+        else if (explode && timeToExplosion <= 0)
+        {
+            Destroy(FirstEnemy);
+            Destroy(gameObject);
+        }
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Enemy" && FirstEnemy == null)
+        {
+            GetComponent<CapsuleCollider>().radius = 50;
+            FirstEnemy = other.gameObject;
+            explode = true;
+            
+        } else {
+            if (other.gameObject.tag == "Enemy")
+            {
+                GameObject bullet = Instantiate(miniBullet, FirstEnemy.transform.position, Quaternion.identity);
+                bullet.GetComponent<BulletAttack>().speed = 2f;
+                bullet.GetComponent<BulletAttack>().target = other.gameObject;
+                Debug.Log("SPAWNED BULLET!");
+            }
+        }
+    }
+
+}
