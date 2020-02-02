e5f3f052edf554ea1d5c50b638807f1ea6ddaba9
zikiro <mrzombiekilla24@yahoo.com>
2020-02-02 15:32:50 -0500

    Main Scene
    - Spawners now are set to active after your robot has legs confirmed.

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -6,21 +6,27 @@ using BrokenBattleBots;
 public class GameManager : MonoBehaviour
 {
     public GameObject[] spawners;
+    public bool StartFighting;
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartFighting = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (BattleBotCustomization.instance.Standing)
+        if (BattleBotCustomization.instance.Standing && !StartFighting)
         {
             foreach (var item in spawners)
             {
-                item.gameObject.SetActive(true);
+                if (item != null)
+                {
+                    item.gameObject.SetActive(true);
+                }
+                
             }
+            StartFighting = true;
         }
         
     }
